e9351cab35ba7ac73df2b0287712da6d3f5ecd7a
SJ Miller <sj.miller.tn@gmail.com>
2023-05-03 03:59:59 -0400

    Frontend styling + fixes (#6)
    * stylesssss bitchessss
    
    * weird fixes for rles who is a btich

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component, useState } from 'react';
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './custom.css';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import Login from './components/Login';
 import Home from './components/Home';
 import View from './components/View';
@@ -17,17 +17,32 @@ function App() {
 
   return (
     <div>
-      <h1>Art Gallery</h1>
-      <p>{token.username}</p>
-      <button type="submit" onClick={e => setToken(null)}>Logout</button>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home username={username} />} />
-          <Route path="/view" element={<View username={username} />} />
-          <Route path="/curate" element={<Curate username={username} />} />
-          <Route path="/donate" element={<Donate username={username} />} />
-        </Routes>
-      </BrowserRouter>
+      <div className='pageWrapper'>
+        <BrowserRouter>
+          <div className='nav cardBg'>
+            <h1>Art Gallery</h1>
+            <div className='navlink'>
+              <Link to="/">
+                <button>Home</button>
+              </Link>
+            </div>
+            <div className='navlink'>
+              <Link to="/donate">
+                <button>Donate Art</button>
+              </Link>
+            </div>
+            <div className='navlink'>
+              <button type="submit" onClick={e => setToken(null)}>Logout</button>
+            </div>
+          </div>
+          <Routes>
+            <Route index element={<Home username={username} />} />
+            <Route path="/view" element={<View username={username} />} />
+            <Route path="/curate" element={<Curate username={username} />} />
+            <Route path="/donate" element={<Donate username={username} />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
